301d5c5cc2ecd52396593fa388e5c6416c3b568f
Burhan <burhanorucov@gmail.com>
2025-06-15 18:18:18 +0400

    Update ExpertDocumentProgram.cs

--- CSharp_DocumentProgram_hw18/Models/ExpertDocumentProgram.cs
+++ CSharp_DocumentProgram_hw18/Models/ExpertDocumentProgram.cs
@@ -2,8 +2,6 @@ namespace CSharp_DocumentProgram_hw18.Models;
 
 public class ExpertDocumentProgram:ProDocumentProgram
 {
-   
-
     public override void SaveDocument()=>Console.WriteLine("Document Saved in pdf format");
     
 }
